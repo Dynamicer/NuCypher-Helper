81e2a1be15d57534f15137a0def61121a67bb4c2
Kay Feng <i@dynamicer.com>
2020-10-05 21:34:56 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,9 +2,7 @@
 sudo apt-get update && sudo apt upgrade -y
 sudo apt install git
 echo -n "请输入密码"
-read NUCYPHER_KEYRING_PASSWORD
-echo -n "请再次输入密码"
-read accountpassword
+read ALLINONEPWD
 cd $HOME
 sudo apt update
 sudo apt upgrade -y
@@ -19,9 +17,9 @@ sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get install ethereum -y
 cd /root/ && mkdir ethereum
 nohup geth --rinkeby --syncmode "light" --datadir "/root/ethereum/" --cache=512 >/dev/null 2>&1 &
-echo -e "$accountpassword\n$accountpassword" | geth --datadir /root/ethereum/ account new
+echo -e "$ALLINONEPWD\n$ALLINONEPWD" | geth --datadir /root/ethereum/ account new
 source nucypher-venv/bin/activate
-export NUCYPHER_KEYRING_PASSWORD=$NUCYPHER_KEYRING_PASSWORD && export NUCYPHER_WORKER_ETH_PASSWORD=$accountpassword
+export NUCYPHER_KEYRING_PASSWORD=$ALLINONEPWD && export NUCYPHER_WORKER_ETH_PASSWORD=$ALLINONEPWD
 echo -e '0\ny\n' | nucypher ursula init --provider ipc:/root/ethereum/geth.ipc --poa --network ibex
 nohup nucypher ursula run --provider ipc:/root/ethereum/geth.ipc --poa --network ibex > ursula_worker.log 2>&1 &
 exit 0
